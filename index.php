9208429e6239473c0b208e3125e751001a23514f
David Petersen <dpete106@gmail.com>
2017-10-12 15:16:10 -0400

    release 1

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Livewater Farm is a Vermont grass-based dairy operation sustainably producing organic farm and dairy products.">
+    <meta name="keywords" content="vermont, farm, dairy, organic, cheese, butter, milk, grassfed">
+    <meta name="author" content="livewaterfarm.net">
     <link rel="icon" href="/livewater/favicon.ico">
 
     <title>Livewater Farm & Dairy</title>
@@ -12,14 +13,35 @@
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 	<script src="http://localhost/livewater/bootstrap-4-dist/js/bootstrap.min.js"></script>
-
+	
+	<!-- <script src="http://livewaterfarm.net/bootstrap-4-dist/js/bootstrap.min.js"></script> -->
+	
     <!-- Bootstrap core CSS -->
     <link href="http://localhost/livewater/bootstrap-4-dist/css/bootstrap.min.css" rel="stylesheet">
+	
+    <!-- <link href="http://livewaterfarm.net/bootstrap-4-dist/css/bootstrap.min.css" rel="stylesheet"> -->
 
     <!-- Custom styles for this template -->
     <link href="http://localhost/livewater/css/narrow-jumbotron.css" rel="stylesheet">
     <link href="http://localhost/livewater/css/navbar.css" rel="stylesheet">
     <link href="http://localhost/livewater/css/cover.css" rel="stylesheet">
+	
+    <!-- <link href="http://livewaterfarm.net/css/narrow-jumbotron.css" rel="stylesheet">
+    <link href="http://livewaterfarm.net/css/navbar.css" rel="stylesheet">
+    <link href="http://livewaterfarm.net/css/cover.css" rel="stylesheet"> -->
+	
+		<!-- Google Analytics-->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-75621382-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
   </head>
 
   <body>
@@ -35,9 +57,14 @@
               <h3 class="masthead-brand">Livewater Farm & Dairy</h3>
               <nav class="nav nav-masthead">
                 <a class="nav-link active" href="#">Home</a>
+				
                 <a class="nav-link" href="/livewater/barnyard/">Blog</a>
                 <a class="nav-link" href="/livewater/stand/">Shop Farm Stand</a>
-              </nav>
+				
+                <!-- <a class="nav-link" href="/barnyard/">Blog</a>
+                <a class="nav-link" href="/stand/">Shop Farm Stand</a> -->
+				
+             </nav>
             </div>
           </div>
 
@@ -45,7 +72,11 @@
             <h3 class="cover-heading">Sustainable Organic Practice since 1980</h3>
             <p class="lead">Livewater Farm is a grass-based dairy operation producing 100% grass-fed raw cow’s milk, in addition to fresh grass-fed beef, pasture-raised chicken, milk-fed pork, free range eggs, garden vegetables, maple syrup, canned goods, and a wide variety of other sustainably produced farm products. In 2015, Livewater Dairy was established as a farmstead dairy processor located on Livewater Farm. Using only the freshest, 100% grass-fed milk produced on site by Livewater Farm, Livewater Dairy produces fresh cheese, aged raw milk cheese, and sweet cream butter.</p>
             <p class="lead">
+			
               <a href="/livewater/barnyard/" class="btn btn-lg btn-secondary">Enter the Barnyard</a>
+			  
+              <!-- <a href="/barnyard/" class="btn btn-lg btn-secondary">Enter the Barnyard</a> -->
+			  
             </p>
 		</div>
 
